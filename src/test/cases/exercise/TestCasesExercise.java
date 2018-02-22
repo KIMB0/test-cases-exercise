514e3a0d075219613177c2f23af25cc75239e402
KIMB0 <Kimsen1992@gmail.com>
2018-02-22 18:27:19 +0100

    Initial of Tines project.

---
+++ src/test/cases/exercise/TestCasesExercise.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.cases.exercise;
+
+/**
+ *
+ * @author Kim
+ */
+public class TestCasesExercise {
+
+
+    public static void main(String[] args) {
+        MyArrayListWithBugs malwb = new MyArrayListWithBugs();
+   }
+    
+}
